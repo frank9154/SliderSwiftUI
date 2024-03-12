3ba5408595f17d4e7eb180955a37cb8e225212c7
alexsokolov <frank9154@gmail.com>
2024-03-12 23:47:37 +0300

    Initial Commit

---
+++ SliderSwiftUI/SliderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderSwiftUIApp.swift
+//  SliderSwiftUI
+//
+//  Created by Александр Соколов on 12.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SliderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
